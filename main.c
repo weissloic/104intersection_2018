8584826df158eec0955a1b4c4fe29223cb325450
Weiss Loïc <wloic@localhost.localdomain>
2019-01-02 16:01:21 +0100

    Cone addind

--- main.c
+++ main.c
@@ -39,7 +39,7 @@ int main(int ac, char **av)
     int argv6 = atoi(av[6]);
     int argv7 = atoi(av[7]);
     int parameter = atoi(av[8]);
-    float rayon = (atoi(av[8]) * M_PI) / 180;
+    float rayon = (M_PI / 2 - parameter * M_PI / 180);
     float racine_1;
     float racine_2;
     float delta;
@@ -76,21 +76,22 @@ int main(int ac, char **av)
 
 
     else if (deter == 3) {
-
-        a_calc = pow(argv5, 2) + pow(argv6, 2) - (pow(argv7, 2) / pow(tan(rayon), 2));
-        b_calc = (2 * argv2 * argv5) + (2 * argv3 * argv6) - (2 * argv4 * argv7 / pow(tan(rayon), 2));
-        c_calc = pow(argv2, 2) + pow(argv3, 2) - pow(argv4, 2) / pow(tan(rayon), 2);
+        printf("Cylinder of radius %d\n", parameter);
+        printf("Line passing throught the point (%d, %d, %d) and parallel to the vector (%d, %d, %d)\n", argv2, argv3, argv4, argv5, argv6, argv7);
+        a_calc = pow(tan(M_PI / 2 - parameter * M_PI / 180), 2) * (pow(argv5, 2) + pow(argv6, 2)) - pow(argv7, 2);
+        b_calc = pow(tan(M_PI / 2 - parameter * M_PI / 180), 2) * (2 * argv2 * argv5 + 2 * argv3 * argv6) - (2 * argv4 * argv7);
+        c_calc = pow(tan(M_PI / 2 - parameter * M_PI / 180), 2) * (pow(argv2, 2) + pow(argv3, 2)) - pow(argv4, 2);
     }
     else 
         exit (84);
 
     delta = pow(b_calc, 2) - (4 * a_calc * c_calc);
 
-    if (delta <= 0 && deter == 2)
+    /*if (delta <= 0 && deter == 2)
         printf("Point infinit");
         printf("Cone with a %d degree angle\n", parameter);
         printf("Line passing throught the point (%d, %d, %d) and parallel to the vector (%d, %d, %d)\n", argv2, argv3, argv4, argv5, argv6, argv7);
-    
+*/
     if (delta == 0) {
         printf("1 intersection point:\n");
         racine_1 = -b_calc / (2 * a_calc);
@@ -101,8 +102,8 @@ int main(int ac, char **av)
 
     else if (delta > 0) {
         printf("2 intersection points:\n");
-        racine_1 = -b_calc - sqrt(delta) / (2 * a_calc);
-        racine_2 = (-b_calc + sqrt(delta) / (2 * a_calc));
+        racine_1 = (-b_calc - sqrt(delta)) / (2 * a_calc);
+        racine_2 = (-b_calc + sqrt(delta)) / (2 * a_calc);
         printf("(%.3f, %.3f, %.3f)\n", argv2 + racine_2 * argv5, argv3 + racine_2 * argv6, argv4 + (racine_2 * argv7));
         printf("(%.3f, %.3f, %.3f)\n", argv2 + racine_1 * argv5, argv3 + racine_1 * argv6, argv4 + (racine_1 * argv7));
         
